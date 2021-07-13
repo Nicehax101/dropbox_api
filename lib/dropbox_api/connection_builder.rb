72e785d9390e3dea2fb50512906c00375b0c56fb
Tomek <tomek.drazek@me.com>
2021-07-13 10:05:38 +0200

    Implements #81 Refresh tokens support

--- lib/dropbox_api/connection_builder.rb
+++ lib/dropbox_api/connection_builder.rb
@@ -12,8 +12,7 @@ module DropboxApi
     def build(url)
       Faraday.new(url) do |connection|
         middleware.apply(connection) do
-          connection.authorization :Bearer, @oauth_bearer
-
+          connection.authorization :Bearer, @oauth_bearer.is_a?(DropboxApi::Token) ? @oauth_bearer.short_lived_token : @oauth_bearer
           yield connection
         end
       end
